2db79ea93d555e811c93e0aaf2d8ee6c9a590adb
Slavyanchik <seroff.s97@gmail.com>
2025-04-22 12:59:31 +0300

    feat : tests for colorful log

--- colorful_handler_test.go
+++ colorful_handler_test.go
@@ -1,3 +1,85 @@
 package slogslav
 
-// TODO I suppose I should track if there an ansi color descriptor and then the off color descriptor
+import (
+	"bytes"
+	"github.com/fatih/color"
+	"log/slog"
+	"os"
+	"testing"
+)
+
+const (
+	withColor = "color"
+	noColor   = "nocolor"
+)
+
+// Let's use for example red
+var (
+	colorOff = []byte("\033[0m")
+	colorRed = []byte("\033[0;31m")
+)
+
+type MockColorWriter struct {
+	*bytes.Buffer
+}
+
+func (m *MockColorWriter) Check() bool {
+	if m.Buffer == nil {
+		return false
+	}
+	return bytes.Index(m.Bytes(), colorRed) != -1 || bytes.Index(m.Bytes(), colorOff) != -1 || bytes.Index(m.Bytes(), colorOff) > bytes.Index(m.Bytes(), colorRed)
+}
+
+func Test_coloring(t *testing.T) {
+	for _, test := range []struct {
+		name    string
+		log     func(logger *slog.Logger)
+		handler string
+		result  bool
+	}{
+		{
+			"color applied ansi found",
+			func(logger *slog.Logger) {
+				logger.Debug("some log")
+				logger.Info("some log")
+			},
+			"color",
+			true,
+		},
+		{
+			"color not applied ansi not found",
+			func(logger *slog.Logger) {
+				logger.Debug("some log")
+				logger.Info("some log")
+			},
+			"nocolor",
+			false,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			var mcw = MockColorWriter{&bytes.Buffer{}}
+			logColor := slog.New(NewColorfulTextHandler(
+				mcw,
+				&ColorFormatterPalette{
+					err:       color.New(color.BgRed),
+					debug:     color.New(color.BgRed),
+					info:      color.New(color.BgRed),
+					warn:      color.New(color.BgRed),
+					time:      color.New(color.BgRed),
+					separator: ' '},
+				nil))
+			logNoColor := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+			switch test.handler {
+			case withColor:
+				test.log(logColor)
+			case noColor:
+				test.log(logNoColor)
+			}
+
+			if mcw.Check() != test.result {
+				t.Errorf("got %v, want %v", mcw.Check(), test.result)
+			}
+		})
+	}
+}
